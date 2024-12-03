f2c10819d3364bb790a3726e85e2146424b5488e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-03 17:43:20 +0530

    Update Currency.cs

--- src/Bookify.Domain/Shared/ValueObjects/Currency.cs
+++ src/Bookify.Domain/Shared/ValueObjects/Currency.cs
@@ -9,4 +9,15 @@ public sealed record Currency
     private Currency(string code) => Code = code;
 
     public string Code { get; init; }
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code) ?? throw new ApplicationException("The currency code is invalid");
+    }
+
+    public static readonly IReadOnlyCollection<Currency> All =
+    [
+        Usd,
+        Eur
+    ];
 }

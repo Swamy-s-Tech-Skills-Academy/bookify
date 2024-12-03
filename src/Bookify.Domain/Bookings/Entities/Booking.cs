7c0f2592ddcc240d24864f9e29689fc683a13b87
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-03 18:25:20 +0530

    Bookings

--- src/Bookify.Domain/Bookings/Entities/Booking.cs
+++ src/Bookify.Domain/Bookings/Entities/Booking.cs
@@ -1,11 +1,52 @@
 ﻿using Bookify.Domain.Abstractions.Entities;
+using Bookify.Domain.Bookings.Enums;
+using Bookify.Domain.Bookings.ValueObjects;
+using Bookify.Domain.Shared.ValueObjects;
 
 namespace Bookify.Domain.Bookings.Entities;
 
 public sealed class Booking : Entity
 {
 
-    public Booking(Guid id) : base(id)
+    private Booking(Guid id, Guid apartmentId, Guid userId, DateRange duration, Money priceForPeriod,
+        Money cleaningFee, Money amenitiesUpCharge, Money totalPrice, BookingStatus status, DateTime createdOnUtc)
+        : base(id)
     {
+        ApartmentId = apartmentId;
+        UserId = userId;
+        Duration = duration;
+        PriceForPeriod = priceForPeriod;
+        CleaningFee = cleaningFee;
+        AmenitiesUpCharge = amenitiesUpCharge;
+        TotalPrice = totalPrice;
+        Status = status;
+        CreatedOnUtc = createdOnUtc;
     }
+
+
+    public Guid ApartmentId { get; private set; }
+
+    public Guid UserId { get; private set; }
+
+    public DateRange Duration { get; private set; }
+
+    public Money PriceForPeriod { get; private set; }
+
+    public Money CleaningFee { get; private set; }
+
+    public Money AmenitiesUpCharge { get; private set; }
+
+    public Money TotalPrice { get; private set; }
+
+    public BookingStatus Status { get; private set; }
+
+    public DateTime CreatedOnUtc { get; private set; }
+
+    public DateTime? ConfirmedOnUtc { get; private set; }
+
+    public DateTime? RejectedOnUtc { get; private set; }
+
+    public DateTime? CompletedOnUtc { get; private set; }
+
+    public DateTime? CancelledOnUtc { get; private set; }
 }

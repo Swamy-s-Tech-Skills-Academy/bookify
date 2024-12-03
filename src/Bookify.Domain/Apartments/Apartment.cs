6d40c462c9584a413d1f01ee57b44bbef3cb10ac
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-03 17:13:45 +0530

    Apartment

---
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,30 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public sealed class Apartment
+{
+    public Guid Id { get; private set; }
+
+    public string Name { get; private set; } = string.Empty;
+
+    public string Description { get; private set; } = string.Empty;
+
+    public string Country { get; private set; } = string.Empty;
+
+    public string State { get; private set; } = string.Empty;
+
+    public string ZipCode { get; private set; } = string.Empty;
+
+    public string City { get; private set; } = string.Empty;
+
+    public string Street { get; private set; } = string.Empty;
+
+    public decimal PriceAmount { get; private set; }
+
+    public string PriceCurrency { get; private set; } = string.Empty;
+
+    public decimal CleaningFeeAmount { get; private set; }
+
+    public string CleaningFeeCurrency { get; private set; } = string.Empty;
+
+    public DateTime? LastBookedOnUtc { get; internal set; }
+}

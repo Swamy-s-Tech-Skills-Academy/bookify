7560b5179321e8f1368592f315f296a5d1474149
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-03 18:21:10 +0530

    Create Booking.cs

---
+++ src/Bookify.Domain/Bookings/Entities/Booking.cs
@@ -0,0 +1,11 @@
+﻿using Bookify.Domain.Abstractions.Entities;
+
+namespace Bookify.Domain.Bookings.Entities;
+
+public sealed class Booking : Entity
+{
+
+    public Booking(Guid id) : base(id)
+    {
+    }
+}

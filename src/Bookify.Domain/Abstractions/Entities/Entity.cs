85022fe9e78a0188b6c4294159a4befac68df220
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-03 18:04:27 +0530

    Domain Event Interface

--- src/Bookify.Domain/Abstractions/Entities/Entity.cs
+++ src/Bookify.Domain/Abstractions/Entities/Entity.cs
@@ -1,11 +1,30 @@
-﻿namespace Bookify.Domain.Abstractions.Entities;
+﻿using Bookify.Domain.Abstractions.Interfaces;
+
+namespace Bookify.Domain.Abstractions.Entities;
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = [];
+
     protected Entity(Guid id)
     {
         Id = id;
     }
 
     public Guid Id { get; init; }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return [.. _domainEvents];
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
 }

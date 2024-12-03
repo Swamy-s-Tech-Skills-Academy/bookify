c5b82e81d5a688ab5e76ce6ba577657cf28398d8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-03 17:57:49 +0530

    Update User.cs

--- src/Bookify.Domain/Users/Entities/User.cs
+++ src/Bookify.Domain/Users/Entities/User.cs
@@ -18,4 +18,11 @@ public sealed class User : Entity
     public LastName LastName { get; private set; }
 
     public Email Email { get; private set; }
+
+    public static User Create(FirstName firstName, LastName lastName, Email email)
+    {
+        var user = new User(Guid.NewGuid(), firstName, lastName, email);
+
+        return user;
+    }
 }
